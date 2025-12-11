455d166ed581b647ef8ac441a72dd690203496b4
DevilDublin <devanshul1429@gmail.com>
2025-12-11 22:19:07 +0000

    Update site.js

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -95,4 +95,22 @@ document.addEventListener("DOMContentLoaded", () => {
             contactForm.reset();
         });
     }
+    document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("themeToggle");
+
+    // Load saved theme
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+
+    toggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+  });
 });
